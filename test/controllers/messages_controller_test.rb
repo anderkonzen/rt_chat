4f6e2251c785f109e8783f1612b126915c96fe5b
Anderson Konzen <anderson.konzen@gmail.com>
2017-02-28 11:15:43 -0300

    Fix tests

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:anderson)
+  end
+  
+  test "should not get index when user not authenticated" do
+    get messages_url
+    assert_redirected_to login_url
+  end
+
   test "should get index" do
-    get messages_index_url
+    login_as @user
+    get messages_url
     assert_response :success
   end
 
-  test "should get create" do
-    get messages_create_url
-    assert_response :success
+  def login_as(user, password: "password")
+    post login_url, params: { session: { username: user.username,
+                                         password: password } }
   end
 
 end
